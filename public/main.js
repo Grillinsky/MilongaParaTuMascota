e161c2c0087a011da8ad5a7c3f005bb278e88d26
Martin Grillo <65568633+Grillinsky@users.noreply.github.com>
2021-10-19 20:26:16 -0300

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -34,7 +34,7 @@ let products = [
 },
 {
     "titulo" : "Pedigree Razas Pequeñas",
-    "precio" : 220,
+    "precio" : 240,
     "imagen" : "/Css/Imagenes/PedigreeRazasPequeñas.png",
     "alt" : "Comida para perro adulto razas pequeñas Pedigree",
     "modal-option-title" : "Kilos",
@@ -58,7 +58,7 @@ let products = [
 },
 {
     "titulo" : "VitalCan Cachorro",
-    "precio" : 200,
+    "precio" : 210,
     "imagen" : "/Css/Imagenes/VitalcanCachorro.png",
     "alt" : "Comida para perro cachorro VitalCan",
     "modal-option-title" : "Kilos",
@@ -82,7 +82,7 @@ let products = [
 },
 {
     "titulo" : "Raza Adulto",
-    "precio" : 140,
+    "precio" : 160,
     "imagen" : "/Css/Imagenes/RazaAdultos.png",
     "alt" : "Comida para perro adulto Raza",
     "modal-option-title" : "Kilos",
@@ -90,7 +90,7 @@ let products = [
 },
 {
     "titulo" : "Raza Cachorros",
-    "precio" : 150,
+    "precio" : 170,
     "imagen" : "/Css/Imagenes/RazaCachorros.png",
     "alt" : "Comida para perros cachorros Raza",
     "modal-option-title" : "Kilos",
@@ -98,7 +98,7 @@ let products = [
 },
 {
     "titulo" : "Pedigree Pouch",
-    "precio" : 240,
+    "precio" : 80,
     "imagen" : "/Css/Imagenes/PedigreePouch.png",
     "alt" : "Pouch o sobre de comida humeda para perro Pedigree",
     "modal-option-value" : "Sobre/s de 100g",
@@ -113,10 +113,81 @@ let products = [
     "modal-option-values" : [3, 6, 10]
 },
 {
-    "titulo" : "DogChow Adulto",
-    "precio" : 240,
-    "imagen" : "/Css/Imagenes/DogChowAdultos.png",
-    "alt" : "Comida para perro adulto DogChow",
+    "titulo" : "Gati Adulto",
+    "precio" : 210,
+    "imagen" : "/Css/Imagenes/GatiAdultos.png",
+    "alt" : "Comida para gato adulto Gati",
+    "modal-option-title" : "Kilos",
+    "modal-option-values" : [1, 2, 3, 4]
+},
+{
+    "titulo" : "Whiskas Adulto",
+    "precio" : 280,
+    "imagen" : "/Css/Imagenes/WhiskasAdulto.png",
+    "alt" : "Comida para gato adulto Whiskas",
+    "modal-option-title" : "Kilos",
+    "modal-option-values" : [1, 2, 3, 4]
+},{
+    "titulo" : "Whiskas Gatitos",
+    "precio" : 280,
+    "imagen" : "/Css/Imagenes/WhiskasGatito.png",
+    "alt" : "Comida para gatito Whiskas",
+    "modal-option-title" : "Kilos",
+    "modal-option-values" : [1, 2, 3, 4]
+},{
+    "titulo" : "Excellent Gato Adulto",
+    "precio" : 440,
+    "imagen" : "/Css/Imagenes/ExcellentGatoAdulto.png",
+    "alt" : "Comida para gato adulo Excellent",
+    "modal-option-title" : "Kilos",
+    "modal-option-values" : [1, 2, 3, 4]
+},{
+    "titulo" : "Raza Gatos",
+    "precio" : 190,
+    "imagen" : "/Css/Imagenes/RazaGatos.png",
+    "alt" : "Comida para gato adulto Raza",
+    "modal-option-title" : "Kilos",
+    "modal-option-values" : [1, 2, 3, 4]
+},{
+    "titulo" : "Whiskas Pouch",
+    "precio" : 80,
+    "imagen" : "/Css/Imagenes/WhiskasPouch.png",
+    "alt" : "Pouch o sobre de comida humeda para gato Whiskas",
+    "modal-option-title" : "Sobre/s de 100g",
+    "modal-option-values" : [1, 2, 3, 4]
+},{
+    "titulo" : "BedyWood Colhon Sanitario",
+    "precio" : 110,
+    "imagen" : "/Css/Imagenes/BedyWood.png",
+    "alt" : "Colchon sanitarias para gato marca BedyWood",
+    "modal-option-title" : "Bolsa/s de 1.8kg",
+    "modal-option-values" : [1, 2, 3, 4]
+},{
+    "titulo" : "Piedras Sanitarias SaniCat",
+    "precio" : 120,
+    "imagen" : "/Css/Imagenes/SaniCat.png",
+    "alt" : "Piedras Sanitarias SaniCat",
+    "modal-option-title" : "Bolsa/s de 2kg",
+    "modal-option-values" : [1, 2, 3, 4]
+},{
+    "titulo" : "Conejin",
+    "precio" : 110,
+    "imagen" : "/Css/Imagenes/MezclaConejo.png",
+    "alt" : "Conejin o comida compactada para conejos",
+    "modal-option-title" : "Kilos",
+    "modal-option-values" : [1, 2, 3, 4]
+},{
+    "titulo" : "Mezcla para Gallinas",
+    "precio" : 70,
+    "imagen" : "/Css/Imagenes/MezclaGallina.jpg",
+    "alt" : "Mezcla de semillas, alimento para gallina",
+    "modal-option-title" : "Kilos",
+    "modal-option-values" : [1, 2, 3, 4]
+},{
+    "titulo" : "CatChow Adultos",
+    "precio" : 350,
+    "imagen" : "/Css/Imagenes/CatChow.png",
+    "alt" : "Comida para gatos marca CatChow",
     "modal-option-title" : "Kilos",
     "modal-option-values" : [1, 2, 3, 4]
 },
@@ -142,7 +213,8 @@ let comidaGato2Btn = document.getElementById("comidaGato2Btn")
 let comidaGato3Btn = document.getElementById("comidaGato3Btn")
 let comidaGato4Btn = document.getElementById("comidaGato4Btn")
 let comidaGato5Btn = document.getElementById("comidaGato5Btn")
-let comidaGato6Btn = document.getElementById("comidaGato6Btb")
+let comidaGato6Btn = document.getElementById("comidaGato6Btn")
+let comidaGato7Btn = document.getElementById("comidaGato7Btn")
 let piedras1Btn = document.getElementById("piedras1Btn");
 let piedras2Btn = document.getElementById("piedras2Btn");
 let conejinBtn = document.getElementById("conejinBtn");
@@ -171,6 +243,18 @@ let razaAdultos = products[8];
 let razaCachorros = products[9];
 let pedigreePouch = products[10];
 let palitosSnackPerro = products[11];
+let gatiAdultos = products[12];
+let whiskasAdulto = products[13];
+let whiskasGatito = products[14];
+let excellentGatoAdulto = products[15];
+let razaGatos = products[16];
+let catChowAdultos = products[22];
+let whiskasPouch = products[17];
+let bedywood = products[18];
+let sanicat = products[19];
+let conejin = products[20];
+let mezclaGallina = products[21];
+
 //                   The Close Button 
     let modal = document.querySelectorAll(".modal");
     let closeBtn = document.getElementsByClassName("close");
@@ -336,6 +420,140 @@ comidaPerro12Btn.addEventListener("click",() =>{
     modalOption4.style.display = "none";
 
 });
+comidaGato1Btn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = gatiAdultos.titulo;
+    productModalImg.src = gatiAdultos.imagen;
+    productModalImg.alt = gatiAdultos.alt;
+    modalOptionTitle.innerHTML = gatiAdultos["modal-option-title"];
+    modalOption1.innerHTML = gatiAdultos["modal-option-values"][0].toString();
+    modalOption2.innerHTML = gatiAdultos["modal-option-values"][1].toString();
+    modalOption3.innerHTML = gatiAdultos["modal-option-values"][2].toString();
+    modalOption4.innerHTML = gatiAdultos["moda-optioin-values"][3].toString();
+
+});
+comidaGato2Btn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = whiskasAdulto.titulo;
+    productModalImg.src = whiskasAdulto.imagen;
+    productModalImg.alt = whiskasAdulto.alt;
+    modalOptionTitle.innerHTML = whiskasAdulto["modal-option-title"];
+    modalOption1.innerHTML = whiskasAdulto["modal-option-values"][0].toString();
+    modalOption2.innerHTML = whiskasAdulto["modal-option-values"][1].toString();
+    modalOption3.innerHTML = whiskasAdulto["modal-option-values"][2].toString();
+    modalOption4.innerHTML = whiskasAdulto["moda-optioin-values"][3].toString();
+
+});
+comidaGato3Btn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = whiskasGatito.titulo;
+    productModalImg.src = whiskasGatito.imagen;
+    productModalImg.alt = whiskasGatito.alt;
+    modalOptionTitle.innerHTML = whiskasGatito["modal-option-title"];
+    modalOption1.innerHTML = whiskasGatito["modal-option-values"][0].toString();
+    modalOption2.innerHTML = whiskasGatito["modal-option-values"][1].toString();
+    modalOption3.innerHTML = whiskasGatito["modal-option-values"][2].toString();
+    modalOption4.innerHTML = whiskasGatito["moda-optioin-values"][3].toString();
+
+});
+comidaGato4Btn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = excellentGatoAdulto.titulo;
+    productModalImg.src = excellentGatoAdulto.imagen;
+    productModalImg.alt = excellentGatoAdulto.alt;
+    modalOptionTitle.innerHTML = excellentGatoAdulto["modal-option-title"];
+    modalOption1.innerHTML = excellentGatoAdulto["modal-option-values"][0].toString();
+    modalOption2.innerHTML = excellentGatoAdulto["modal-option-values"][1].toString();
+    modalOption3.innerHTML = excellentGatoAdulto["modal-option-values"][2].toString();
+    modalOption4.innerHTML = excellentGatoAdulto["moda-optioin-values"][3].toString();
+
+});
+comidaGato5Btn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = razaGatos.titulo;
+    productModalImg.src = razaGatos.imagen;
+    productModalImg.alt = razaGatos.alt;
+    modalOptionTitle.innerHTML = razaGatos["modal-option-title"];
+    modalOption1.innerHTML = razaGatos["modal-option-values"][0].toString();
+    modalOption2.innerHTML = razaGatos["modal-option-values"][1].toString();
+    modalOption3.innerHTML = razaGatos["modal-option-values"][2].toString();
+    modalOption4.innerHTML = razaGatos["moda-optioin-values"][3].toString();
+});
+comidaGato6Btn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = whiskasPouch.titulo;
+    productModalImg.src = whiskasPouch.imagen;
+    productModalImg.alt = whiskasPouch.alt;
+    modalOptionTitle.innerHTML = whiskasPouch["modal-option-title"];
+    modalOption1.innerHTML = whiskasPouch["modal-option-values"][0].toString();
+    modalOption2.innerHTML = whiskasPouch["modal-option-values"][1].toString();
+    modalOption3.innerHTML = whiskasPouch["modal-option-values"][2].toString();
+    modalOption4.innerHTML = whiskasPouch["moda-optioin-values"][3].toString();
+
+});
+comidaGato7Btn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = catChowAdultos.titulo;
+    productModalImg.src = catChowAdultos.imagen;
+    productModalImg.alt = catChowAdultos.alt;
+    modalOptionTitle.innerHTML = catChowAdultos["modal-option-title"];
+    modalOption1.innerHTML = catChowAdultos["modal-option-values"][0].toString();
+    modalOption2.innerHTML = catChowAdultos["modal-option-values"][1].toString();
+    modalOption3.innerHTML = catChowAdultos["modal-option-values"][2].toString();
+    modalOption4.innerHTML = catChowAdultos["moda-optioin-values"][3].toString();
+
+});
+piedras1Btn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = bedywood.titulo;
+    productModalImg.src = bedywood.imagen;
+    productModalImg.alt = bedywood.alt;
+    modalOptionTitle.innerHTML = bedywood["modal-option-title"];
+    modalOption1.innerHTML = bedywood["modal-option-values"][0].toString();
+    modalOption2.innerHTML = bedywood["modal-option-values"][1].toString();
+    modalOption3.innerHTML = bedywood["modal-option-values"][2].toString();
+    modalOption4.style.display = "none";
+
+});
+piedras2Btn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = sanicat.titulo;
+    productModalImg.src = sanicat.imagen;
+    productModalImg.alt = sanicat.alt;
+    modalOptionTitle.innerHTML = sanicat["modal-option-title"];
+    modalOption1.innerHTML = sanicat["modal-option-values"][0].toString();
+    modalOption2.innerHTML = sanicat["modal-option-values"][1].toString();
+    modalOption3.innerHTML = sanicat["modal-option-values"][2].toString();
+    modalOption4.style.display = "none";
+
+});
+
+mezclaGallinaBtn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = mezclaGallina.titulo;
+    productModalImg.src = mezclaGallina.imagen;
+    productModalImg.alt = mezclaGallina.alt;
+    modalOptionTitle.innerHTML = mezclaGallina["modal-option-title"];
+    modalOption1.innerHTML = mezclaGallina["modal-option-values"][0].toString();
+    modalOption2.innerHTML = mezclaGallina["modal-option-values"][1].toString();
+    modalOption3.innerHTML = mezclaGallina["modal-option-values"][2].toString();
+    modalOption4.innerHTML = mezclaGallina["moda-optioin-values"][3].toString();
+
+});
+
+conejinBtn.addEventListener("click",() =>{
+    showModal();
+    productModalTitle.innerHTML = conejin.titulo;
+    productModalImg.src = conejin.imagen;
+    productModalImg.alt = conejin.alt;
+    modalOptionTitle.innerHTML = conejin["modal-option-title"];
+    modalOption1.innerHTML = conejin["modal-option-values"][0].toString();
+    modalOption2.innerHTML = conejin["modal-option-values"][1].toString();
+    modalOption3.innerHTML = conejin["modal-option-values"][2].toString();
+    modalOption4.innerHTML = conejin["moda-optioin-values"][3].toString();
+
+});
+
 
 
 
